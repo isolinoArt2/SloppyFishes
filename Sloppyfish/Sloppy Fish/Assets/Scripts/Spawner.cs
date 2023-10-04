1de53293643bd487887407d1a8f9346523ef33d9
isolin0 <46123824+isolin0@users.noreply.github.com>
2023-10-04 16:01:59 -0300

    isolino Cambios 1

--- Sloppyfish/Sloppy Fish/Assets/Scripts/Spawner.cs
+++ Sloppyfish/Sloppy Fish/Assets/Scripts/Spawner.cs
@@ -5,19 +5,17 @@ using UnityEngine;
 public class Spawner : MonoBehaviour
 {
     public List<GameObject> prefabs; // Lista de prefabs inicial
-    public List<GameObject> prefabs1; // Lista de prefabs inicial
-    public List<GameObject> prefabs2; // Lista de prefabs para la segunda etapa
-    public List<GameObject> prefabs3; // Lista de prefabs para la tercera etapa
     public List<Transform> spawnpoints; // Lista de puntos de spawn
     public float tiempoEntreInstancias = 2f; // Tiempo en segundos entre cada instancia
 
     private float playingTime = 0f; // Tiempo transcurrido desde el inicio del juego
+    private int ultimoPrefabInstanciadoIndex = -1; // Índice del último prefab instanciadopr
+    private int _nuevoIndice;
 
     private void Start()
     {
         // Comenzar a invocar la función Spawn() cada tiempoEntreInstancias segundos
         InvokeRepeating("Spawn", 0f, tiempoEntreInstancias);
-        prefabs = prefabs1;
     }
 
     private void Update()
@@ -25,20 +23,33 @@ public class Spawner : MonoBehaviour
         // Actualizar el tiempo de juego
         playingTime += Time.deltaTime;
 
-        // Verificar la dificultad y cambiar las listas de prefabs
-        if (playingTime > 30f && playingTime <= 60f)
+        // Verificar si hay al menos dos prefabs en la lista
+        if (prefabs.Count >= 2)
         {
-            // Cambiar a la lista de prefabs2
-            prefabs = prefabs2;
-           // Debug.Log("segundo nivel");
+            // Seleccionar un nuevo índice aleatorio para el próximo prefab
+            int nuevoIndice = Random.Range(0, prefabs.Count);
+
+            // Verificar si hay un último prefab instanciado
+            if (ultimoPrefabInstanciadoIndex != -1)
+            {
+                // Limitar el rango del índice aleatorio en función del último prefab instanciado
+                int minIndex = Mathf.Max(0, ultimoPrefabInstanciadoIndex - 1);
+                int maxIndex = Mathf.Min(prefabs.Count - 1, ultimoPrefabInstanciadoIndex + 1);
+
+                // Seleccionar un nuevo índice aleatorio dentro del rango permitido
+                nuevoIndice = Random.Range(minIndex, maxIndex + 1);
+               _nuevoIndice = nuevoIndice ;
+            }
+
+
+            
         }
-        else if (playingTime > 60f)
+        else
         {
-            // Cambiar a la lista de prefabs3
-            prefabs = prefabs3;
-           // Debug.Log("segundo nivel");
+            Debug.LogWarning("La lista de prefabs debe contener al menos dos elementos para funcionar correctamente.");
         }
     }
+
     public void ResetTimer()
     {
         playingTime = 0;
@@ -58,11 +69,12 @@ public class Spawner : MonoBehaviour
         int indicePrefabAleatorio = Random.Range(0, prefabs.Count);
         GameObject prefabSeleccionado = prefabs[indicePrefabAleatorio];
 
-        // Seleccionar un punto de spawn aleatorio de la lista
+        // Instanciar el prefab en la posición del punto de spawn seleccionado
         int indiceSpawnAleatorio = Random.Range(0, spawnpoints.Count);
         Transform puntoSpawnSeleccionado = spawnpoints[indiceSpawnAleatorio];
+        Instantiate(prefabs[_nuevoIndice], puntoSpawnSeleccionado.position, puntoSpawnSeleccionado.rotation);
 
-        // Instanciar el prefab en la posición del punto de spawn seleccionado
-        Instantiate(prefabSeleccionado, puntoSpawnSeleccionado.position, puntoSpawnSeleccionado.rotation);
+        // Actualizar el índice del último prefab instanciado
+        ultimoPrefabInstanciadoIndex = _nuevoIndice;
     }
 }
